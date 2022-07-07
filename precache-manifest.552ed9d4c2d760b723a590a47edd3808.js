9cce01db51ae7a1abd2fc9376095cb4171e578ca
DESKTOP-P7P3MBD\Admin <vinod.patil377@gmail.com>
2022-07-07 16:55:47 +0530

    Updates

---
+++ precache-manifest.552ed9d4c2d760b723a590a47edd3808.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f587b49c3e4226106935",
+    "url": "/better_clean/static/css/main.91458570.chunk.css"
+  },
+  {
+    "revision": "f587b49c3e4226106935",
+    "url": "/better_clean/static/js/main.f587b49c.chunk.js"
+  },
+  {
+    "revision": "2e26c9e59951c6a1550c",
+    "url": "/better_clean/static/css/1.51749f51.chunk.css"
+  },
+  {
+    "revision": "2e26c9e59951c6a1550c",
+    "url": "/better_clean/static/js/1.2e26c9e5.chunk.js"
+  },
+  {
+    "revision": "4f6f90b9582932e2a50c",
+    "url": "/better_clean/static/js/runtime~main.4f6f90b9.js"
+  },
+  {
+    "revision": "12e2b38a7d21d647bf5f6de39ce144eb",
+    "url": "/better_clean/static/media/cleaning_service.12e2b38a.png"
+  },
+  {
+    "revision": "61749d1d76edf70d8e964cd690be1733",
+    "url": "/better_clean/static/media/customer_review.61749d1d.png"
+  },
+  {
+    "revision": "ae83ba2c8d2ca2c1b524a94f9d5d67d9",
+    "url": "/better_clean/static/media/wel1.ae83ba2c.png"
+  },
+  {
+    "revision": "713968bb3d5d278871d79540a81e9369",
+    "url": "/better_clean/static/media/wel2.713968bb.jpg"
+  },
+  {
+    "revision": "7f3e0b74111a588338350341e8d418db",
+    "url": "/better_clean/static/media/about.7f3e0b74.jpg"
+  },
+  {
+    "revision": "4ad2995724b8dca722f8f0f52f80df7d",
+    "url": "/better_clean/static/media/contact.4ad29957.png"
+  },
+  {
+    "revision": "4193dc8046b9d5dfbc935c174b61505e",
+    "url": "/better_clean/static/media/hero.4193dc80.png"
+  },
+  {
+    "revision": "3bcc0f0b8d8e4877ff26fa4f8ce8b527",
+    "url": "/better_clean/index.html"
+  }
+];
